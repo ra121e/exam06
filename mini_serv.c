c4260949463272353eb5dfaba94ca5f1a3889d78
Atsushi Honda <athonda@c1r6s1.42singapore.sg>
2026-01-15 17:17:01 +0800

    feat: port check
      _ acceptable port might be 1024 to 49152

--- mini_serv.c
+++ mini_serv.c
@@ -3,10 +3,18 @@
 
 int	main(int ac, char **av)
 {
+	int	port;
+
 	if (ac != 2)
 	{
 		write (2, "Wrong number of arguments\n", 26);
 	}
+	port = atoi(av[1]);
+	if (!(port > 1024 && port < 49151))
+	{
+		write (2, "Fatal error\n", 12);
+	}
+
 
 	return (0);
 }
