31ec169f7bef68b2756ee7a4bbcfa482a2424352
Atsushi Honda <athonda@c1r6s1.42singapore.sg>
2026-01-17 19:57:38 +0800

    fix: exit instead of return in case wrong ac

--- mini_serv.c
+++ mini_serv.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <unistd.h>
 
 
@@ -8,12 +9,17 @@ int	main(int ac, char **av)
 	if (ac != 2)
 	{
 		write (2, "Wrong number of arguments\n", 26);
+		exit (1);
 	}
+
 	port = atoi(av[1]);
 	if (!(port > 1024 && port < 49151))
 	{
 		write (2, "Fatal error\n", 12);
+		return (1);
 	}
+
+
 
 
 	return (0);
