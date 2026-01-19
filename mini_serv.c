c45f39f4693143d25fca23e444945ea65931fa76
root <sushi.honda@gmail.com>
2026-01-19 08:50:25 +0800

    feat: make sockfd by socket()
      - before make socket fd, activefd fd_set should be zero clean by FD_ZERO
      - then, open socket
      - At last, set the socket fd into acrtivefd bit pattern by FD_SET

--- mini_serv.c
+++ mini_serv.c
@@ -2,10 +2,13 @@
 #include <unistd.h> // read, write
 #include <sys/select.h> //fd_set
 #include <sys/socket.h> // socket, SOMAXCONN
+#include <strings.h> // bzero
+#include <arpa/inet.h> // hotonl
 
 int	main(int ac, char **av)
 {
 	int	port;
+	int		sockfd;
 	fd_set	activefd;
 	fd_set	readfd;
 	fd_set	writefd;
@@ -24,6 +27,16 @@ int	main(int ac, char **av)
 		exit (1);
 	}
 
+	FD_ZERO(&activefd);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		write (2, "Fatal error\n", 12);
+		exit (1);
+	}
+	FD_SET(sockfd, &activefd);
+
+
 
 	struct sockaddr_in servaddr; 
 	bzero(&servaddr, sizeof(servaddr)); 
@@ -31,7 +44,7 @@ int	main(int ac, char **av)
 	// assign IP, PORT 
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(av[1]); 
+	servaddr.sin_port = htons(atoi(av[1])); 
 
 	// Binding newly created socket to given IP and verification 
 	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0) { 
